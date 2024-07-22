67862ec25cb3c63411d25455da387f762212057f
alexander-zhong <alexanderzousky@gmail.com>
2024-07-21 21:46:27 -0700

    enable http-only jwt for auth and register and implemented logout (erasing auth cookie)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,21 @@ import generateToken from "../utils/generateToken.js";
 // POST /api/users/auth
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Auth user" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.checkPassword(password))) {
+    generateToken(res, user._id);
+    return res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 // @desc Register User
@@ -43,7 +57,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // POST /api/users/logout
 // @access Public
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Logout user" });
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "User logged out" });
 });
 
 // @desc Get User's profile
